a91f1b4ee8663adf0f8c9372eceade9dd6af0da0
cebarrico <cbarrico@gmail.com>
2023-05-02 14:56:04 -0300

    feat:DRE and model copies

--- books/models.py
+++ books/models.py
@@ -1,47 +1,47 @@
 from django.db import models
 
+
 class Genre(models.TextChoices):
-    FANTASY = 'Fantasy'
-    ROMANCE = 'Romance'
-    SCIENCE_FICTION = 'Science Fiction'
-    HORROR = 'Horror'
-    MYSTERY = 'Mystery'
-    ADVENTURE = 'Adventure'
-    BIOGRAPHY = 'Biography'
-    CLASSICS = 'Classics'
-    DRAMA = 'Drama'
-    FAMILY = 'Family'
-    HISTORY = 'History'
-    HISTORY_FICTION = 'History Fiction'
-    LITERARY_FICTION = 'Literary Fiction'
-    MUSICAL = 'Musical'
-    RELIGION = 'Religion'
-    SATIRE = 'Satire'
-    SOCIETY = 'Society'
-    THRILLER = 'Thriller'
-    WESTERN = 'Western'
-    UNDEFINED = 'Undefined'
+    FANTASY = "Fantasy"
+    ROMANCE = "Romance"
+    SCIENCE_FICTION = "Science Fiction"
+    HORROR = "Horror"
+    MYSTERY = "Mystery"
+    ADVENTURE = "Adventure"
+    BIOGRAPHY = "Biography"
+    CLASSICS = "Classics"
+    DRAMA = "Drama"
+    FAMILY = "Family"
+    HISTORY = "History"
+    HISTORY_FICTION = "History Fiction"
+    LITERARY_FICTION = "Literary Fiction"
+    MUSICAL = "Musical"
+    RELIGION = "Religion"
+    SATIRE = "Satire"
+    SOCIETY = "Society"
+    THRILLER = "Thriller"
+    WESTERN = "Western"
+    UNDEFINED = "Undefined"
+
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     author = models.CharField(max_length=255)
-    genre = models.CharField(choices=Genre.choices, default=Genre.UNDEFINED, max_length=50)
+    genre = models.CharField(
+        choices=Genre.choices, default=Genre.UNDEFINED, max_length=50
+    )
     pages = models.IntegerField()
     isbn = models.CharField(max_length=13, unique=True)
-    amount = models.IntegerField()
+    copies_amount = models.IntegerField()
     book_cover = models.TextField()
-    loans = models.ManyToManyField("users.User", through="Loan", related_name='loans')
-    followers = models.ManyToManyField("users.User", through="Follow", related_name='following')
-    
-class Loan(models.Model):
-    book =  models.ForeignKey('books.Book', on_delete=models.CASCADE)
-    user =  models.ForeignKey('users.User', on_delete=models.CASCADE)
-    borrowed_at = models.DateTimeField(auto_now_add=True)
-    return_date = models.DateTimeField()
-    returned = models.BooleanField(default=False)
+
+    followers = models.ManyToManyField(
+        "users.User", through="Follow", related_name="following"
+    )
+
 
 class Follow(models.Model):
-    book =  models.ForeignKey('books.Book', on_delete=models.CASCADE)
-    user =  models.ForeignKey('users.User', on_delete=models.CASCADE)
+    book = models.ForeignKey("books.Book", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     status = models.BooleanField(default=False)
---
+++ copies/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Copy(models.Model):
+    book = models.ForeignKey("books.Book", on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+    loans = models.ManyToManyField("users.User", through="Loan", related_name="loans")
+
+
+class Loan(models.Model):
+    copy = models.ForeignKey("copies.Copy", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    borrowed_at = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField()
+    returned = models.BooleanField(default=False)
