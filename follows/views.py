ad0621bf1b7df9fd8e91afd8645e542445d6b703
unknown <arthurnaocopia@gmail.com>
2023-05-03 15:08:38 -0300

    fix/Permissions & Book View conflicts

--- follows/views.py
+++ follows/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from .models import Book
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from library_bookend.permissions import isEmployeeOrReadOnly
 
-# Create your views here.
+
+class FollowDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        book = Book.objects.get(pk=pk)
+        return Response({"count": book.followers.count(), "followers": book.followers.all()})
+
+    def post(self, request, pk):
+        book = Book.objects.get(pk=pk)
+        book.followers.add(request.user)
+        return Response(status=200)
+
+    def delete(self, request, pk):
+        book = Book.objects.get(pk=pk)
+        book.followers.remove(request.user)
+        return Response(status=204)
