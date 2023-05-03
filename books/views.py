573e0020173ef4979fc811db63352fe24ddd23b7
Renata Costa <renata_rayanne_@hotmail.com>
2023-05-02 21:08:58 -0300

    feat: createBook

--- books/views.py
+++ books/views.py
@@ -1,1 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from users.permissions import isEmployee
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [isEmployee]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
